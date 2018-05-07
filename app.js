64dc0aef96ab4dc0b305e8c31d0ffe14464d9f20
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-07 11:48:50 -0400

    Nodes

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const wmf = document.querySelector('#book-list li:nth-child(2) .name');
 
 
 
-var books = document.querySelectorAll('#book-list li .name');
+const banner = document.querySelector('#page-banner');
 
-books.forEach(book => {
-    book.textContent += ' (book title)';
-});
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const bookList = document.querySelector('#book-list');
+const clonedBanner = banner.cloneNode(true);
 
-//bookList.innerHTML = '<h2> Books and books and nooks</h2>';
-bookList.innerHTML += '<p>This is how you add</p>'
+console.log(clonedBanner);
