7749cf4a0e7f2c1873c269ab46d5dae315a91540
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-07 11:53:53 -0400

    DOM traversl pt1

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
+const bookList = document.querySelector('#book-list');
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent node is:', bookList.parentElement.parentElement);
 
-
-
-const banner = document.querySelector('#page-banner');
-
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes:', banner.hasChildNodes());
-
-const clonedBanner = banner.cloneNode(true);
-
-console.log(clonedBanner);
+console.log(bookList.children);
