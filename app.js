431a618ab96af6e6410a4b4fa0b10468a305e561
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-05-07 18:29:34 -0400

    Custom Search Filter

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ addForm.addEventListener('submit', function(e){
     list.appendChild(li);
 });
 
-// hide box
+// hide book
 
 const hideBox = document.querySelector('#hide');
 
@@ -46,3 +46,18 @@ hideBox.addEventListener('change', function(e){
     }
 })
 
+// filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    
+   Array.from(books).forEach(book => {
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term) != -1) {
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    })
+});
